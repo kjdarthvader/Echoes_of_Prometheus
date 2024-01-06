c4214cc2e63724d1befcee7144fc96a8e16bbcbd
Kavin Jindel <115748304+kjdarthvader@users.noreply.github.com>
2024-01-06 17:10:32 +0530

    Create assistant.py

---
+++ assistant.py
@@ -0,0 +1,9 @@
+import openai
+import gtts
+import speech_recognition as sr
+from playsound import playsound
+import pyaudio
+import pygame 
+import uuid
+import io
+from tempfile import NamedTemporaryFile
